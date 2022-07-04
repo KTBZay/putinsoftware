4454f0719a25f1061ea46a134c1268a0d4c4da59
KTBZay <97396492+KTBZay@users.noreply.github.com>
2022-07-04 15:20:45 -0400

    Delete putin_modules/Client/ClientCreator/utils directory

--- putin_modules/Client/ClientCreator/utils/about.js
+++
@@ -1,6 +0,0 @@
-const About = {
-    Description: 'Hi im Zay, the developer of this bot, i create discord bots for a hobbey, some go popular and some dont. \n Getting started with the bot: blue = information, red = error, yellow = possible error but ignored \n Modules: \n EmailTransporter'
-}
-module.exports = {
-    About
-}
